9fbb428ed03b39f715bd567cfcc84a0d67d30895
dtmkeng <kang081@hotmail.com>
2019-08-05 09:03:39 +0700

    update allocation heap

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ server_bin_name="gowebbenchmark"
 length=${#web_frameworks[@]}
 
 test_result=()
-
+test_alloc_result=()
 cpu_cores=`cat /proc/cpuinfo|grep processor|wc -l`
 if [ $cpu_cores -eq 0 ]
 then
@@ -17,13 +17,15 @@ fi
 test_web_framework()
 {
   echo "testing web framework: $2"
-  ./$server_bin_name $2 $3 &
+  ./$server_bin_name $2 $3  > alloc.log 2>&1 &
   sleep 2
 
   throughput=`wrk -t$cpu_cores -c$4 -d30s http://127.0.0.1:8081/hello | grep Requests/sec | awk '{print $2}'`
+  alloc=`cat alloc.log|grep HeapAlloc | awk '{print $2}'`
   echo "throughput: $throughput requests/second"
   echo "cpu_cores: $cpu_cores"
   test_result[$1]=$throughput
+  test_alloc_result[$1]=$alloc
 
   pkill -9 $server_bin_name
   sleep 2
@@ -47,9 +49,10 @@ test_all()
 
 
 pkill -9 $server_bin_name
-
+echo ","$(IFS=$','; echo "${web_frameworks[*]}" ) > processtime_alloc.csv
 echo ","$(IFS=$','; echo "${web_frameworks[*]}" ) > processtime.csv
 test_all 0 100
 echo "0 ms,"$(IFS=$','; echo "${test_result[*]}" ) >> processtime.csv
+echo "0 ms,"$(IFS=$','; echo "${test_alloc_result[*]}" ) >> processtime_alloc.csv
 
 
